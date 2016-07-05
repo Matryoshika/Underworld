ddd919425af397adffffd9260dd98885b3f15e69
Matryoshika <jonathancarlsson159@gmail.com>
2016-07-05 23:01:36 +0200

    Main file

--- src/main/java/se/Matryoshika/Underworld/Underworld.java
+++ src/main/java/se/Matryoshika/Underworld/Underworld.java
@@ -3,6 +3,8 @@ package se.Matryoshika.Underworld;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.DimensionType;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -13,6 +15,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import se.Matryoshika.Underworld.Utils.CreativeTabUnderworld;
+import se.Matryoshika.Underworld.WorldGen.WorldProviderCaves;
+import se.Matryoshika.Underworld.WorldGen.WorldTypeCaves;
 
 @Mod(modid=Underworld.MODID, version=Underworld.VERSION, name="Underworld")
 public class Underworld {
@@ -20,6 +24,10 @@ public class Underworld {
 	public static final String MODID = "underworld";
 	public static final String LOCALIZING = "UW";
 	public static final String VERSION = "0.01";
+	
+	
+	public static WorldTypeCaves worldTypeCaves;
+	
 	
 	public static final CreativeTabUnderworld UnderworldTab = new CreativeTabUnderworld("Underworld"){
 		@Override
@@ -39,9 +47,17 @@ public class Underworld {
 	public void preInit(FMLPreInitializationEvent event){}
 	
 	@EventHandler
-	public void Init(FMLInitializationEvent event){}
+	public void Init(FMLInitializationEvent event){
+		
+		DimensionManager.unregisterDimension(0);
+		DimensionManager.registerDimension(0, DimensionType.register("CAVES", "WhatIsTHis", 0, WorldProviderCaves.class, true));
+	}
 	
 	@EventHandler
-	public void postInit(FMLPostInitializationEvent event){}
+	public void postInit(FMLPostInitializationEvent event){
+		
+		worldTypeCaves = new WorldTypeCaves();
+		
+	}
 
 }
