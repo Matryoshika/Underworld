0ec0f4d068ecc3d7f1cccffbbda1c0f7f6019ff9
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-03 13:15:23 +0200

    Proxies, bane of my existance

--- src/main/java/se/Matryoshika/Underworld/ClientProxy.java
+++ src/main/java/se/Matryoshika/Underworld/ClientProxy.java
@@ -5,22 +5,26 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import se.Matryoshika.Underworld.Content.BlockRegistry;
 import se.Matryoshika.Underworld.Content.Rendering.BlockRenderRegister;
+import se.Matryoshika.Underworld.Content.Rendering.ItemRenderRegister;
 
-public class ClientProxy implements IProxy{
+public class ClientProxy extends CommonProxy{
 	
 	@Override
-	public void preInit(){
+	public void preInit(FMLPreInitializationEvent event){
 		BlockRenderRegister.registerBlockRenderer();
+		ItemRenderRegister.registerItemRenderer();
+		
 	}
 	
 	@Override
-	public void init(){
-
+	public void init(FMLInitializationEvent event){
+		
     }
 	
 	@Override
-    public void postInit(){
+    public void postInit(FMLPostInitializationEvent event){
 
     }
 
--- src/main/java/se/Matryoshika/Underworld/CommonProxy.java
+++ src/main/java/se/Matryoshika/Underworld/CommonProxy.java
@@ -4,18 +4,20 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import se.Matryoshika.Underworld.Content.BlockRegistry;
+import se.Matryoshika.Underworld.Content.ItemRegistry;
 
 public class CommonProxy {
 	
-	public void preInit(FMLPreInitializationEvent e) {
-		BlockRegistry.createBlocks();
+	public void preInit(FMLPreInitializationEvent event){
+		
     }
 
-    public void init(FMLInitializationEvent e) {
+    public void init(FMLInitializationEvent event) {
 
     }
+	
 
-    public void postInit(FMLPostInitializationEvent e) {
+    public void postInit(FMLPostInitializationEvent event) {
 
     }
 
