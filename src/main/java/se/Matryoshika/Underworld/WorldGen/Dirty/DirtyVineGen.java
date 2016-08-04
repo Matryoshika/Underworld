e00f4a55a80168c6e822c35ba659c061a34baad2
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-04 04:37:20 +0200

    Removing dumb check that always blocked spawning

--- src/main/java/se/Matryoshika/Underworld/WorldGen/Dirty/DirtyVineGen.java
+++ src/main/java/se/Matryoshika/Underworld/WorldGen/Dirty/DirtyVineGen.java
@@ -66,12 +66,6 @@ private void generateStructure(World world, int x, int y, int z, Random rand) {
 		Random genRand = new Random();
 		Random shortCircuit = new Random();
 		
-		//Only spawn in warm biomes
-		Biome biome = world.getChunkFromBlockCoords(new BlockPos(x,y,z)).getBiome(new BlockPos(x,y,z), world.getBiomeProvider());
-		if(biome.getTempCategory() != Biome.TempCategory.COLD || biome.getTempCategory() != Biome.TempCategory.OCEAN){
-			return;
-		}
-		
 		if(genRand.nextInt(4) == 0){
 			
 			
