4f203c716302c24a254f373dbac65bb785137ca8
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-07 18:19:45 +0200

    Tiny cleanup

--- src/main/java/se/Matryoshika/Underworld/WorldGen/WorldProviderCaves.java
+++ src/main/java/se/Matryoshika/Underworld/WorldGen/WorldProviderCaves.java
@@ -18,37 +18,15 @@ public class WorldProviderCaves extends WorldProvider{
 	}
 	
 	@Override
-	public void setSpawnPoint(BlockPos pos){
-		if(this.worldObj.getWorldType() instanceof WorldTypeCaves){
-			worldObj.getWorldInfo().setSpawn(new BlockPos(0,50,0));
-		}
-		else{
-			worldObj.getWorldInfo().setSpawn(pos);
-		}
-    }
-	
-	@Override
-	public BlockPos getSpawnCoordinate(){
-		if(this.worldObj.getWorldType() instanceof WorldTypeCaves){
-			return new BlockPos(0,51,0);
-		}
-		else{
-			return null;
-		}
-	}
-	
-	@Override
 	public BlockPos getSpawnPoint(){
-        if(this.worldObj.getWorldType() instanceof WorldTypeCaves){
-        	return new BlockPos(0, 51, 0);
-		}
-		else{
-			net.minecraft.world.storage.WorldInfo info = worldObj.getWorldInfo();
-	        return new BlockPos(info.getSpawnX(), info.getSpawnY(), info.getSpawnZ());
-		}
+		net.minecraft.world.storage.WorldInfo info = worldObj.getWorldInfo();
+		BlockPos spawn = this.worldObj.getWorldType() instanceof WorldTypeCaves ? new BlockPos(0, 51, 0) : new BlockPos(info.getSpawnX(), info.getSpawnY(), info.getSpawnZ());
+		
+		return spawn;
         
     }
 	
+	@Override
 	public boolean getHasNoSky(){
 		
 		return this.worldObj.getWorldType() instanceof WorldTypeCaves ? true : this.hasNoSky;
@@ -63,20 +41,19 @@ public class WorldProviderCaves extends WorldProvider{
      * Called to determine if the chunk at the given chunk coordinates within the provider's world can be dropped. Used
      * in WorldProviderSurface to prevent spawn chunks from being unloaded.
      */
-    public boolean canDropChunk(int x, int z)
-    {
+	@Override
+    public boolean canDropChunk(int x, int z){
         return !this.worldObj.isSpawnChunk(x, z) || !this.worldObj.provider.getDimensionType().shouldLoadSpawn();
     }
 
     /**
      * Creates the light to brightness table
      */
-    protected void generateLightBrightnessTable()
-    {
+	@Override
+    protected void generateLightBrightnessTable(){
     	float f = 0.0F;
 
-        for (int i = 0; i <= 15; ++i)
-        {
+        for (int i = 0; i <= 15; ++i){
             float f1 = 1.0F - (float)i / 15.0F;
             this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * (1.0F - f) + f;
         }
@@ -86,39 +63,37 @@ public class WorldProviderCaves extends WorldProvider{
     /**
      * Returns 'true' if in the "main surface world", but 'false' if in the Nether or End dimensions.
      */
-    public boolean isSurfaceWorld()
-    {
+	@Override
+    public boolean isSurfaceWorld(){
         return true;
     }
 
     /**
      * Will check if the x, z position specified is alright to be set as the map spawn point
      */
-    public boolean canCoordinateBeSpawn(int x, int z)
-    {
+	@Override
+    public boolean canCoordinateBeSpawn(int x, int z){
         return true;
     }
 
     /**
      * Calculates the angle of sun and moon in the sky relative to a specified time (usually worldTime)
      */
-    public float calculateCelestialAngle(long worldTime, float partialTicks)
-    {
+	@Override
+    public float calculateCelestialAngle(long worldTime, float partialTicks){
     	int i = (int)(worldTime % 24000L);
         float f = ((float)i + partialTicks) / 24000.0F - 0.25F;
 
-        if (f < 0.0F)
-        {
+        if (f < 0.0F){
             ++f;
         }
 
-        if (f > 1.0F)
-        {
+        if (f > 1.0F){
             --f;
         }
 
         float f1 = 1.0F - (float)((Math.cos((double)f * Math.PI) + 1.0D) / 2.0D);
-        f = f + (f1 - f) / 3.0F;
+        f += (f1 - f) / 3.0F;
     	
     	return this.worldObj.getWorldType() instanceof WorldTypeCaves ? 0.50F : f;
     }
@@ -126,8 +101,8 @@ public class WorldProviderCaves extends WorldProvider{
     /**
      * True if the player can respawn in this dimension (true = overworld, false = nether).
      */
-    public boolean canRespawnHere()
-    {
+	@Override
+    public boolean canRespawnHere(){
         return true;
     }
 
@@ -135,28 +110,23 @@ public class WorldProviderCaves extends WorldProvider{
      * Returns true if the given X,Z coordinate should show environmental fog.
      */
     @SideOnly(Side.CLIENT)
-    public boolean doesXZShowFog(int x, int z)
-    {
+    public boolean doesXZShowFog(int x, int z){
         return false;
     }
 
     /**
      * Returns the dimension's name, e.g. "The End", "Nether", or "Overworld".
      */
-    public String getDimensionName()
-    {
+    public String getDimensionName(){
         return "Overworld";
     }
     @Override
-    public int getHeight()
-    {
+    public int getHeight(){
     	return this.worldObj.getWorldType() instanceof WorldTypeCaves ? 128 : 256;
-
     }
     
     @Override
-    public int getActualHeight()
-    {
+    public int getActualHeight(){
     	return hasNoSky ? 128 : 256;
     }
 
