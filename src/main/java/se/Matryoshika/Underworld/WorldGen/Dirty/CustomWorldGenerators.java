276bed1324cd409f418b1d26b587cdcc3de83b49
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-08 18:03:18 +0200

    Cleaning up WorldGenerator registry

---
+++ src/main/java/se/Matryoshika/Underworld/WorldGen/Dirty/CustomWorldGenerators.java
@@ -0,0 +1,25 @@
+package se.Matryoshika.Underworld.WorldGen.Dirty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraftforge.fml.common.IWorldGenerator;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class CustomWorldGenerators {
+	
+	public static void register(){
+		
+		GameRegistry.registerWorldGenerator(new DirtyClayGen(), 49);
+		GameRegistry.registerWorldGenerator(new DirtyTreeGen(), 50);
+		GameRegistry.registerWorldGenerator(new DirtyVineGen(), 51);
+		GameRegistry.registerWorldGenerator(new DirtyHutGen(), 52);
+		GameRegistry.registerWorldGenerator(new DirtyDungeonGen(), 53);
+		GameRegistry.registerWorldGenerator(new DirtySpawnPointGen(), Integer.MAX_VALUE-1);
+		GameRegistry.registerWorldGenerator(new DirtySpawnerGen(), Integer.MAX_VALUE);
+		GameRegistry.registerWorldGenerator(new DirtyLilypadGen(), 54);
+		
+		
+	}
+
+}
