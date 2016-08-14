8419721409367168f066714e3c333e63cdf83b65
Matryoshika <jonathancarlsson159@gmail.com>
2016-08-14 03:41:14 +0200

    Fixing these guys from bleeding into other dimensions / worldtypes

--- src/main/java/se/Matryoshika/Underworld/WorldGen/Dirty/DirtySpawnerGen.java
+++ src/main/java/se/Matryoshika/Underworld/WorldGen/Dirty/DirtySpawnerGen.java
@@ -8,11 +8,20 @@ import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import se.Matryoshika.Underworld.Content.ContentRegistry;
+import se.Matryoshika.Underworld.WorldGen.WorldTypeCaves;
 
 public class DirtySpawnerGen implements IWorldGenerator{
 
 	@Override
 	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+		if (!world.provider.isSurfaceWorld()){
+			//System.out.println("NOT 0! IT IS "+world.provider.getDimension());
+			return;
+		}
+		if(!(world.getWorldType() instanceof WorldTypeCaves)){
+			//System.out.println(world.getWorldType());
+			return;
+		}
 		
 		if(rand.nextInt(8) == 0){
 			world.setBlockState(new BlockPos(chunkX*16, 129, chunkZ*16),  ContentRegistry.Spawner.getDefaultState());
