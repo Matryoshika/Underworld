2456c54379ae2dc162633abd86e60ced1d3d6348
Matryoshika <jonathancarlsson159@gmail.com>
2016-10-01 19:21:25 +0200

    Updating Main

--- src/main/java/se/Matryoshika/Underworld/Underworld.java
+++ src/main/java/se/Matryoshika/Underworld/Underworld.java
@@ -1,11 +1,16 @@
 package se.Matryoshika.Underworld;
 
+import java.io.File;
+
+import com.typesafe.config.Config;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldType;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -16,12 +21,14 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
+import se.Matryoshika.Underworld.API.UnderworldMetamorphicTableRecipes;
 import se.Matryoshika.Underworld.Content.ContentRegistry;
 import se.Matryoshika.Underworld.Content.RecipeManager;
 import se.Matryoshika.Underworld.Content.TileRegistry;
 import se.Matryoshika.Underworld.Events.PlayerTicker;
 import se.Matryoshika.Underworld.Events.UnderworldMapEventHandler;
 import se.Matryoshika.Underworld.Utils.BiomeType;
+import se.Matryoshika.Underworld.Utils.ConfigHandler;
 import se.Matryoshika.Underworld.Utils.CreativeTabUnderworld;
 import se.Matryoshika.Underworld.WorldGen.WorldProviderCaves;
 import se.Matryoshika.Underworld.WorldGen.WorldTypeCaves;
@@ -34,6 +41,10 @@ public class Underworld {
 	public static final String MODID = "underworld";
 	public static final String LOCALIZING = "UW";
 	public static final String VERSION = "0.0.10";
+	public static Configuration mainConfig;
+	public static Configuration itemConfig;
+	public static Configuration blockConfig;
+	public static Configuration genConfig;
 	
 	static WorldType CAVES;
 	
@@ -56,17 +67,30 @@ public class Underworld {
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
+		mainConfig = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() +"/"+ Underworld.MODID+"/main.cfg"));
+		itemConfig = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() +"/"+ Underworld.MODID+"/items.cfg"));
+		blockConfig = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() +"/"+ Underworld.MODID+"/blocks.cfg"));
+		genConfig = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() +"/"+ Underworld.MODID+"/worldGen.cfg"));
+		
+		ConfigHandler.readMain();
+		
+		ContentRegistry.prepareBlocks();
+		ContentRegistry.prepareItems();
+		TileRegistry.registerTiles();
+		
+		ConfigHandler.setItemAndBlockConfigs();
 		ContentRegistry.registerBlocks();
 		ContentRegistry.registerItems();
-		TileRegistry.registerTiles();
+		
 		BiomeType.init();
 		proxy.preInit(event);
-		
 		RecipeManager.registerRecipes();
 		
 		UnderworldStructureOceanMonumentPieces.registerOceanMonumentPieces();
 		MinecraftForge.TERRAIN_GEN_BUS.register(INIT_MAP_GEN_EVENT_HANDLER);
 		MinecraftForge.EVENT_BUS.register(PLAYER_TICKER);
+		
+		UnderworldMetamorphicTableRecipes.init();
 	}
 	
 	@EventHandler
@@ -94,6 +118,10 @@ public class Underworld {
 	
 	public static WorldType getCaves(){
     	return CAVES;
+    }
+	
+	public static Underworld getMod(){
+    	return instance;
     }
 
 }
