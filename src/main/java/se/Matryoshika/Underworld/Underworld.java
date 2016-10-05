246d4929e43641f4eaca2895c538063d5b52b383
Matryoshika <jonathancarlsson159@gmail.com>
2016-10-05 20:09:26 +0200

    Because CurseForge is dum-dum

--- src/main/java/se/Matryoshika/Underworld/Underworld.java
+++ src/main/java/se/Matryoshika/Underworld/Underworld.java
@@ -40,7 +40,7 @@ public class Underworld {
 	
 	public static final String MODID = "underworld";
 	public static final String LOCALIZING = "UW";
-	public static final String VERSION = "0.1.12";
+	public static final String VERSION = "0.1.13";
 	public static Configuration mainConfig;
 	public static Configuration itemConfig;
 	public static Configuration blockConfig;
