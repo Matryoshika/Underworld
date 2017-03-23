d1fc3637c4e124229d7d60e38aad4490d81fbdd1
Matryoshika <jonathancarlsson159@gmail.com>
2017-03-24 00:33:16 +0100

    Fixed the Sugar Pile not rendering particles

--- src/main/java/se/Matryoshika/Underworld/Content/TileEntity/TileSugarPile.java
+++ src/main/java/se/Matryoshika/Underworld/Content/TileEntity/TileSugarPile.java
@@ -3,31 +3,24 @@ package se.Matryoshika.Underworld.Content.TileEntity;
 import java.util.List;
 import java.util.Random;
 
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import se.Matryoshika.Underworld.Underworld;
 import se.Matryoshika.Underworld.Content.ContentRegistry;
-import se.Matryoshika.Underworld.Utils.SugarPileList;
 
 public class TileSugarPile extends TileEntity implements ITickable {
 
 	private int age = 0;
-	
-	public TileSugarPile(){
+
+	public TileSugarPile() {
 	}
 
 	@Override
 	public void update() {
-		if (worldObj.isRemote)
-			return;
+		
 		age++;
 		double x = pos.getX();
 		double y = pos.getY();
@@ -50,11 +43,14 @@ public class TileSugarPile extends TileEntity implements ITickable {
 			if (distance < 1)
 				distance = 1;
 
-			for (int i = 0; i < 3; i++)
-				Underworld.proxy.spawnCustomParticle("firefly", worldObj,
-						entity.posX - 0.4 + (Math.random() - Math.random()),
-						(entity.posY + 1) + (Math.random() * 2 - 1),
-						entity.posZ - 0.4 + (Math.random() - Math.random()), 0.3, 0.3, 0.3, 3, 1, 0.1f, 0.2f);
+			for (int i = 0; i < 1; i++) {
+				double pos1 = entity.posX - 0.4 + (Math.random() - Math.random());
+				double pos2 = (entity.posY + 1) + (Math.random() * 2 - 1);
+				double pos3 = entity.posZ - 0.4 + (Math.random() - Math.random());
+				Underworld.proxy.spawnCustomParticle("firefly", worldObj, pos1, pos2, pos3, 0.3, 0.3, 0.3, 3, 1, 0.1f, 0.2f);
+				
+				//System.out.println(Print.print(pos1, pos2, pos3));
+			}
 
 		}
 		if (age >= 6000) {
