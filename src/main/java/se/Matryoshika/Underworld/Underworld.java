a9220abb010957dfd9f0de66fa417cf3c8101694
Matryoshika <jonathancarlsson159@gmail.com>
2017-03-24 01:45:26 +0100

    Updating to 0.3.12

--- src/main/java/se/Matryoshika/Underworld/Underworld.java
+++ src/main/java/se/Matryoshika/Underworld/Underworld.java
@@ -45,7 +45,7 @@ public class Underworld {
 
 	public static final String MODID = "underworld";
 	public static final String LOCALIZING = "UW";
-	public static final String VERSION = "0.1.12";
+	public static final String VERSION = "0.3.12";
 	public static Configuration mainConfig;
 	public static Configuration itemConfig;
 	public static Configuration blockConfig;
